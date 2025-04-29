7cfd851b142d2947b04c88ded0df371ab9bf052e
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-04-30 04:01:31 +0530

    python

---
+++ python/string/modify_string.py
@@ -0,0 +1,6 @@
+#Upper case
+username = "akash"
+print('Upper Case:',username.upper())
+
+#Lower Case
+fullname = 
---
+++ python/string/slicing.py
@@ -0,0 +1,12 @@
+#    0123456789 
+x = "textstring"
+print(x[1:3])
+
+# Slicing from start
+print('String From Start: ',x[:4])
+
+# Slice To the End
+print("Slice from End: ",x[4:])
+
+# Negative Indexing
+print('Negative Indexing: ',x[-5:-2])
