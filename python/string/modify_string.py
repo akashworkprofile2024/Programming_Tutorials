a87d5ee5ff99d66671569a356d53b56e611b7f71
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-05-01 06:46:30 +0530

    python

--- python/string/modify_string.py
+++ python/string/modify_string.py
@@ -3,4 +3,21 @@ username = "akash"
 print('Upper Case:',username.upper())
 
 #Lower Case
-fullname = 
+fullname = "AKASH"
+print('Lower Case:',fullname.lower())
+
+#Capitalize Case 
+print('Capitalized:',fullname.capitalize())
+
+#Remove Spaces
+address = "123 Main Street"
+print('Address:',address.strip())
+
+#Replace String
+username1 = "Akash Biswas"
+print('Replace:',username1.replace("Akash","Amit"))
+
+#Split String
+array = "Python, Java, C++"
+print(array)
+print('Split:',array.split(","))
