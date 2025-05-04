75eb2e612b2f265c85e62cf9fa047e554cdd7d4f
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-05-04 20:53:33 +0530

    python

---
+++ python/Lists/access_items.py
@@ -0,0 +1,12 @@
+# Access Items in a List
+listarr = ["Python", "Java", "C++", "JavaScript", "Ruby"]
+print('Access Items: ',listarr[3],'\n')  # Accessing the first item
+
+# Negative Indexing
+print('Negative Indexing: ',listarr[-1],'\n')  # Accessing the last item
+
+# Range of Indexes
+print('Range of Indexes [1:4]: ',listarr[1:4],'\n')  # Accessing a range of items
+print('Range of Indexes [:3]: ',listarr[:3],'\n')  # Accessing a range of items from the beginning
+print('Range of Indexes [2:]: ',listarr[2:],'\n')  # Accessing a range of items from the end
+print('Range of Negative Indexes [-4:-1]: ',listarr[-4:-1],'\n')  # Accessing a range of items using negative indexing
---
+++ python/Lists/list_concept.py
@@ -0,0 +1,17 @@
+mylist = ['Python', 'Java', 'C++', 'JavaScript']
+print(mylist)
+print(type(mylist),'\n')  # <class 'list'>
+
+# Allows duplicate values
+mylist = ['Python', 'Java', 'C++', 'JavaScript', 'Python']
+print('Allow Duplicate Values',mylist,'\n')
+
+# List Length
+mylist = ['Python', 'Java', 'C++', 'JavaScript']
+print('Length List: ',len(mylist),'\n')  # 4
+
+# List Constructors
+mylist = list(('Python', 'Java', 'C++', 'JavaScript'))
+print('List Constructor: ',mylist,'\n')  # ['Python', 'Java', 'C++', 'JavaScript']
+
+
