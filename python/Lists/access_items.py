0ea7927dc6269196b67967df247cb1c53859555b
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-05-04 20:55:03 +0530

    python

--- python/Lists/access_items.py
+++ python/Lists/access_items.py
@@ -10,3 +10,10 @@ print('Range of Indexes [1:4]: ',listarr[1:4],'\n')  # Accessing a range of item
 print('Range of Indexes [:3]: ',listarr[:3],'\n')  # Accessing a range of items from the beginning
 print('Range of Indexes [2:]: ',listarr[2:],'\n')  # Accessing a range of items from the end
 print('Range of Negative Indexes [-4:-1]: ',listarr[-4:-1],'\n')  # Accessing a range of items using negative indexing
+
+
+# Check if Item Exists
+if "C++" in  listarr:
+    print('C++ is in the list','\n')
+else:
+    print('C++ is not in the list','\n')    
